87afb775ff1451b868df4f1ac812e3b09375cbbf
Juan M. Solórzano I <55208621+JMSolorzano@users.noreply.github.com>
2022-11-13 09:41:52 -0600

    [FIX] change config to ranger choosedir

--- config.fish
+++ config.fish
@@ -2,10 +2,5 @@ starship init fish | source
 
 # Add this to your ~/.config/fish/config.fish
 function ranger-cd
-    set tmpfile "/tmp/pwd-from-ranger"
-    ranger --choosedir=$tmpfile $argv
-    set rangerpwd (cat $tmpfile)
-    if test "$PWD" != $rangerpwd
-        cd $rangerpwd
-    end
+	ranger --choosedir="$HOME/.rangerdir"; cd (cat $HOME/.rangerdir)
 end
