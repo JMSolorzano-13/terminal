16741781517b393f421e68042a4cd5e5917766c4
Juan M. Solórzano I <55208621+JMSolorzano-13@users.noreply.github.com>
2024-04-10 13:19:34 -0600

    Update ginit.fish

--- functions/ginit.fish
+++ functions/ginit.fish
@@ -1,34 +1,40 @@
 function ginit
-    set branch $argv[1] ;
-    set remote_url $argv[2] ;
+    set branch $argv[1]
+    set remote_url $argv[2]
 
     # Check if all required parameters are provided
     if test -z "$branch" -o -z "$remote_url"
-        echo "Error: All parameters are required: branch, commit message, and remote URL."
+        echo "Error: All parameters are required: branch and remote URL."
+        return 1
+    end
+
+    # Check if current directory is already part of a git repository
+    if git rev-parse --is-inside-work-tree >/dev/null 2>&1
+        echo "Error: Current directory is already inside a git repository."
         return 1
     end
 
     # Initialize a new Git repository
-    git init ;
+    git init
     echo "Initialize git folder."
 
     # Create a new branch and switch to it
-    git checkout -b $branch ;
-    echo "Create and checkuot branch $branch."
+    git checkout -b $branch
+    echo "Create and checkout branch $branch."
 
     # Add all changes
-    git add . ;
+    git add .
     echo "Add all changes to commit."
 
-    # Commit changes with the provided message
-    git commit -m "[INIT]" ;
+    # Commit changes with a default message
+    git commit -m "[INIT]"
     echo "Add message to commit."
 
     # Add the remote repository
-    git remote add origin $remote_url ;
+    git remote add origin $remote_url
     echo "Add remote repository."
 
     # Push changes to the remote repository
-    git push -u origin $branch ;
+    git push -u origin $branch
     echo "Push to new repository, new branch."
 end
